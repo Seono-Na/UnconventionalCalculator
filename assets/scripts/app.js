2ea2375beae75482d4f5f5c7d5106aa9cf6a6294
Seono-Na <seono5670@gmail.com>
2023-11-14 04:17:07 +0900

    버튼 기능 구현

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,52 @@
-alert('This works!');
+const defaultResult = 0;
+var currentResult = defaultResult;
+
+function getUserInputNumber() {
+  return parseFloat(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCal, enteredNumber) {
+  const calDescription = `${resultBeforeCal} ${operator} ${enteredNumber}`;
+  outputResult(currentResult, calDescription);
+}
+
+function resetInput() {
+  userInput.value = "";
+  userInput.focus();
+}
+
+function add() {
+  const enteredNumber = getUserInputNumber();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  resetInput();
+}
+function subtract() {
+  const enteredNumber = getUserInputNumber();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+  resetInput();
+}
+function multiply() {
+  const enteredNumber = getUserInputNumber();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+  resetInput();
+}
+function divide() {
+  const enteredNumber = getUserInputNumber();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+  resetInput();
+}
+
+resetInput();
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
